035141ad6485725c65aa68bde1970ea2d747df2f
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-11-12 02:07:08 -0800

    Create woinpror.R

---
+++ woinpror.R
@@ -0,0 +1,3 @@
+#search "oil sands" on the third result page = "https://patentscope.wipo.int/search/en/result.jsf?currentNavigationRow=next&prevCurrentNavigationRow=3&query=%22oil%20sands%22&office=&sortOption=Pub%20Date%20Desc&prevFilter=&maxRec=8427&viewOption=All"
+#Access the URL at html tag "<td id="resultListForm:j_idt372_itm">" which is "https://patentscope.wipo.int/search/rss.jsf?query=%22oil+sands%22&office=&rss=true&sortOption=Pub+Date+Desc" 
+#which gives the first 300 results for the search in RSS format.
